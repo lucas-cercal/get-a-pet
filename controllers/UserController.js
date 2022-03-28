3cbde6ad0926ffec2bf42b809ba6547997bdb6bc
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-03-28 10:50:15 -0300

    Validações de usuários

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,6 +2,46 @@ const User = require('../models/User');
 
 module.exports = class UserController{
     static async register (req, res){
-        res.json('Olá Get a Pet');
+        const {name, email, phone, password, confirmpassword} = req.body;
+
+        // Validations
+        if(!name){
+            res.status(422).json({message: 'O nome é obrigatório!'});
+            return;    
+        }
+
+        if(!email){
+            res.status(422).json({message: 'O e-mail é obrigatório!'});
+            return;    
+        }
+
+        if(!phone){
+            res.status(422).json({message: 'O telefone é obrigatório!'});
+            return;    
+        }
+
+        if(!password){
+            res.status(422).json({message: 'A senha é obrigatória!'});
+            return;    
+        }
+
+        if(!confirmpassword){
+            res.status(422).json({message: 'A confirmação de senha é obrigatória!'});
+            return;    
+        }
+
+        if(password !== confirmpassword){
+            res.status(422).json({message: 'A senha e a confirmação de senha precisam ser iguais!'});
+            return;    
+        }
+
+        // Check if user exists
+        const userExists = await User.findOne({email: email});
+
+        if(userExists){
+            res.status(422).json({message: 'Por favor, utilize outro e-mail!'});
+            return;
+        }
+
     }
 }
