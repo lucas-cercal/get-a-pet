3ad31597cc25881a721bf28b025993c9392bdae2
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-03-28 17:04:50 -0300

    Pegando token do header

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,6 +1,11 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+// Helpers
 const createUserToken = require('../helpers/create-user-token');
+const getToken = require('../helpers/get-token');
+
 
 module.exports = class UserController{
     static async register (req, res){
@@ -102,7 +107,10 @@ module.exports = class UserController{
         console.log(req.headers.authorization);
 
         if(req.headers.authorization){
-
+            const token  = getToken(req);
+            const decoded = jwt.verify(token, `${process.env.SECRET_VALUE}`);
+            currentUser = await User.findById(decoded.id);
+            currentUser.password = undefined;
         }else{
             currentUser = null;
         }
