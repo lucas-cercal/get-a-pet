6d16fda0c9ec1fdf1aba7701063e52db33edb187
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-03-28 16:47:44 -0300

    Verificando usuário pelo token

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -95,4 +95,18 @@ module.exports = class UserController{
 
         await createUserToken(user, req, res);
     }
+
+    static async checkUser(req, res){
+        let currentUser;
+        
+        console.log(req.headers.authorization);
+
+        if(req.headers.authorization){
+
+        }else{
+            currentUser = null;
+        }
+
+        res.status(200).send(currentUser);
+    }
 }
