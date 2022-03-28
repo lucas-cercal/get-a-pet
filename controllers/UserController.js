85a6fd0838abb16063bd65512826d2c5632e4376
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-03-28 15:31:10 -0300

    Retornando token JWT

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+const createUserToken = require('../helpers/create-user-token');
 
 module.exports = class UserController{
     static async register (req, res){
@@ -58,8 +59,7 @@ module.exports = class UserController{
 
         try {
             const newUser = await user.save();
-            res.status(201).json({message: 'Usuário criado!', newUser});
-            return;
+            await createUserToken(newUser, req, res);
         } catch (error) {
             res.status(500).json({message: error});
         }
