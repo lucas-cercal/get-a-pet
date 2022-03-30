4012dd8fb96fe7e715ed0917c879d9224e55f8a1
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-03-30 14:43:09 -0300

    Função de resgatar todos os pets

--- routes/PetRoutes.js
+++ routes/PetRoutes.js
@@ -7,5 +7,6 @@ const verifyToken = require('../helpers/verify-token');
 const {imageUpload} = require('../helpers/image-upload');
 
 router.post('/create', verifyToken, imageUpload.array('images'), PetController.create);
+router.get('/', PetController.getAll);
 
 module.exports = router;    
