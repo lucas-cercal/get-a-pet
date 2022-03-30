e90528e4b32ca3e14123f3584c092a5847020bf2
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-03-30 15:40:53 -0300

    Resgatando pet pelo id

--- controllers/PetController.js
+++ controllers/PetController.js
@@ -3,7 +3,7 @@ const Pet = require('../models/Pet');
 // Helpers
 const getToken = require('../helpers/get-token');
 const getUserByToken = require('../helpers/get-user-by-token');
-const { restart } = require('nodemon');
+const ObjectId = require('mongoose').Types.ObjectId;
 
 module.exports = class PetController{
     // Create a pet
@@ -102,6 +102,26 @@ module.exports = class PetController{
 
         res.status(200).json({
             pets,
+        })
+    }
+
+    static async getPetById(req, res){
+        const id = req.params.id;
+        
+        if(!ObjectId.isValid(id)){
+            res.status(422).json({message: "ID inválido!"});
+            return;
+        }
+
+        // Check if pet exists
+        const pet = await Pet.findOne({_id: id});
+
+        if(!pet){
+            res.status(404).json({message: "Pet não encontrado!"});
+        }
+
+        res.status(200).json({
+            pet: pet,
         })
     }
 }
