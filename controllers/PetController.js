06634c78658dea18628d60a89346c82f33a889e2
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-03-30 15:11:58 -0300

    Função para resgatar todas as adoções

--- controllers/PetController.js
+++ controllers/PetController.js
@@ -3,6 +3,7 @@ const Pet = require('../models/Pet');
 // Helpers
 const getToken = require('../helpers/get-token');
 const getUserByToken = require('../helpers/get-user-by-token');
+const { restart } = require('nodemon');
 
 module.exports = class PetController{
     // Create a pet
@@ -78,5 +79,17 @@ module.exports = class PetController{
         res.status(200).json({
             pets: pets,
         });
+    }
+
+    static async getAllUserPets(req, res){
+        // Get user from token
+        const token = getToken(req);
+        const user = await getUserByToken(token);
+
+        const pets = await Pet.find({'user._id': user._id}).sort('-createdAt');
+
+        res.status(200).json({
+            pets,
+        })
     }
 }
