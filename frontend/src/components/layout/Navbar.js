2bdf02999da3c42eb5750c7d92dec490505ab3ec
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-03-31 14:12:36 -0300

    CSS na navbar e footer

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -1,11 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
-import Logo from "../../assets/img/logo.png"
+import styles from './Navbar.module.css'
+
+import Logo from '../../assets/img/logo.png'
 
 function Navbar() {
     return (
-       <nav>
-           <div>
+       <nav className={styles.navbar}>
+           <div className={styles.navbar_logo}>
             <img src={Logo} alt="Get A Pet" />
             <h2>Get A Pet</h2>
            </div>
