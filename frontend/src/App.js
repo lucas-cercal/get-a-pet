abc26aff892f4ae94ac294c66a2aaab73729d0cc
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-03-31 13:45:17 -0300

    Estruturando navbar e footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,9 @@
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
+/* COMPONENTS */
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+
 /* PAGES */
 import Login from './components/pages/Auth/Login';
 import Register from './components/pages/Auth/Register';
@@ -8,6 +12,7 @@ import Home from './components/pages/Home';
 function App() {
   return (
     <Router>
+      <Navbar/>
       <Switch>
         <Route path="/login">
           <Login/>
@@ -19,6 +24,7 @@ function App() {
           <Home/>
         </Route>
       </Switch>
+      <Footer/>
     </Router>
   );
 }
