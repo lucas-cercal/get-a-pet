d89bf50011972c123ac0eb08af3622bfd1f4035b
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-03-31 13:34:09 -0300

    Inserindo o React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,25 @@
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+/* PAGES */
+import Login from './components/pages/Auth/Login';
+import Register from './components/pages/Auth/Register';
+import Home from './components/pages/Home';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Get A Pet</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <Login/>
+        </Route>
+        <Route path="/register">
+          <Register/>
+        </Route>
+        <Route path="/">
+          <Home/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
