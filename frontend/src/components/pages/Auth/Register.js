f267079a165f861e830c836731e87e31ed4f042c
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-04-01 10:00:07 -0300

    Criando objeto de usuário

--- frontend/src/components/pages/Auth/Register.js
+++ frontend/src/components/pages/Auth/Register.js
@@ -5,57 +5,65 @@ import {useState} from 'react';
 import styles from "../../form/Form.module.css";
 
 function Register() {
-    const [user, setUser] = useState
+  const [user, setUser] = useState()
 
-    function handleChange(e){
+  function handleChange(e){
+    setUser({...user, [e.target.name]: e.target.value})
+  }
 
-    }
-    return (
-        <section className={styles.form_container}>
-            <h1>Registrar</h1>
-            <form>
-            <Input
-                    text='Nome'
-                    type='text'
-                    name='name'
-                    placeholder='Digite o seu nome'
-                    handleOnChange={handleChange}
-                />
-                <Input
-                    text='Telefone'
-                    type='text'
-                    name='phone'
-                    placeholder='Digite o seu telefone'
-                    handleOnChange={handleChange}
-                /> 
-                <Input
-                    text='E-mail'
-                    type='email'
-                    name='email'
-                    placeholder='Digite o seu email'
-                    handleOnChange={handleChange}
-                /> 
-                <Input
-                    text='Senha'
-                    type='password'
-                    name='password'
-                    placeholder='Digite a sua senha'
-                    handleOnChange={handleChange}
-                /> 
-               <Input
-                    text='Confirmação de senha'
-                    type='password'
-                    name='confirmpassword'
-                    placeholder='Confirme a sua senha'
-                    handleOnChange={handleChange}
-                /> 
-                <input type="submit" value="Cadastrar"/>
-            </form>
-            <p>
-                Já tem conta? <Link to='/login'>Clique aqui!</Link>
-            </p>
-        </section>
-    )
+  function handleSubmit(e){
+    e.preventDefault();
+    // Enviar o usuário para o banco
+    console.log(user);
+
+  }
+
+  return (
+    <section className={styles.form_container}>
+      <h1>Registrar</h1>
+      <form onSubmit={handleSubmit}>
+      <Input
+        text='Nome'
+        type='text'
+        name='name'
+        placeholder='Digite o seu nome'
+        handleOnChange={handleChange}
+      />
+      <Input
+        text='Telefone'
+        type='text'
+        name='phone'
+        placeholder='Digite o seu telefone'
+        handleOnChange={handleChange}
+      /> 
+      <Input
+        text='E-mail'
+        type='email'
+        name='email'
+        placeholder='Digite o seu email'
+        handleOnChange={handleChange}
+      /> 
+      <Input
+        text='Senha'
+        type='password'
+        name='password'
+        placeholder='Digite a sua senha'
+        handleOnChange={handleChange}
+      /> 
+      <Input
+        text='Confirmação de senha'
+        type='password'
+        name='confirmpassword'
+        placeholder='Confirme a sua senha'
+        handleOnChange={handleChange}
+      /> 
+        <input type="submit" value="Cadastrar"/>
+      </form>
+      <p>
+        Já tem conta? <Link to='/login'>Clique aqui!</Link>
+      </p>
+    </section>
+  )
 }
 
 export default Register;
