0449f0c93bb0627bda179866c81dfebe8cbe09eb
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-04-02 12:33:22 -0300

    Formulário de edição de usuário

--- frontend/src/components/pages/User/Profile.js
+++ frontend/src/components/pages/User/Profile.js
@@ -1,7 +1,77 @@
+import styles from './Profile.module.css';
+import formStyles from '../../form/Form.module.css';
+import Input from '../../form/Input';
+
+import {useState, useEffect} from 'react';
+
+
 function Profile() {
+  const [user, setUser] = useState({});
+
+  function onFileChange(e){
+
+  }
+
+  function handleChange(e){
+
+  }
+
   return (
     <section>
-      <h1>Profile</h1>
+      <div className={styles.profile_header}>
+        <h1>Perfil</h1>
+        <p>Preview Imagem</p>
+      </div>
+      <form className={formStyles.form_container}>
+        <Input
+          text="Imagem"
+          type="file"
+          name="image"
+          handleOnChange={onFileChange}
+        />
+        <Input
+          text="Nome"
+          type="text"
+          name="name"
+          placeholder="Digite o seu nome"
+          handleOnChange={handleChange}
+          value={user.name || ''}
+        />
+        <Input
+          text="Telefone"
+          type="text"
+          name="phone"
+          placeholder="Digite o seu telefone"
+          handleOnChange={handleChange}
+          value={user.phone || ''}
+        />
+        <Input
+          text="E-mail"
+          type="email"
+          name="email"
+          placeholder="Digite o seu e-mail"
+          handleOnChange={handleChange}
+          value={user.email || ''}
+        />
+        <Input
+          text="Senha"
+          type="password"
+          name="password"
+          placeholder="Digite a sua senha"
+          handleOnChange={handleChange}
+          value={user.password || ''}
+        />
+        <Input
+          text="Confirmação de senha"
+          type="password"
+          name="confirmpassword"
+          placeholder="Confirme a sua senha"
+          handleOnChange={handleChange}
+        />
+
+        <input type="submit" value="Editar"/>
+        
+      </form>
     </section>
   );
 }
