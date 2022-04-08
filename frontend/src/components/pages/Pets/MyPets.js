b904ed2394f407501653704c50c8eb833340c7f9
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-04-08 16:01:14 -0300

    Exibindo os pets na dashboard

--- frontend/src/components/pages/Pets/MyPets.js
+++ frontend/src/components/pages/Pets/MyPets.js
@@ -1,5 +1,7 @@
 import api from '../../../utils/api'
 
+import styles from './Dashboard.module.css'
+
 import { useState, useEffect } from 'react'
 
 import { Link } from 'react-router-dom'
@@ -32,7 +34,29 @@ export default function MyPets() {
         <Link to="/pet/add">Cadastrar</Link>
       </div>
       <div>
-        {pets.length > 0 ? <p>Meus pets cadastrados</p> : <p>Não há pets cadastrados</p>}
+        {pets.length > 0 ? pets.map((pet) => (
+          <div key={pet._id}>
+            <RoundedImage
+              src={`${process.env.REACT_APP_API}/images/pets/${pet.images[0]}`} 
+              alt={pet.name} 
+              width="75px"
+            />
+            <span className="bold">{pet.name}</span>
+            <div className={styles.action}>
+              {pet.available 
+              ? (
+                <>
+                  {pet.adopter && (
+                    <button>Concluir adoção</button>
+                  )}
+                  <Link to={`/pet/edit/${pet._id}`}>Editar</Link> 
+                  <button>Excluir</button>
+                </>
+              )
+              : (<p>Pet já adotado</p>)}
+            </div>
+          </div>
+        )) : <p>Não há pets cadastrados</p>}
       </div>
     </section>
   );
