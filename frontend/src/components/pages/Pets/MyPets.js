0457276345c3d18348f875386e5e3d5331b0d804
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-04-08 16:31:11 -0300

    CSS da dashboard

--- frontend/src/components/pages/Pets/MyPets.js
+++ frontend/src/components/pages/Pets/MyPets.js
@@ -29,25 +29,25 @@ export default function MyPets() {
 
   return (
     <section>
-      <div>
+      <div className={styles.petlist_header}>
         <h1>MyPets</h1>
-        <Link to="/pet/add">Cadastrar</Link>
+        <Link to="/pet/add">Cadastrar Pet</Link>
       </div>
-      <div>
+      <div className={styles.petlist_container}>
         {pets.length > 0 ? pets.map((pet) => (
-          <div key={pet._id}>
+          <div className={styles.petlist_row} key={pet._id}>
             <RoundedImage
               src={`${process.env.REACT_APP_API}/images/pets/${pet.images[0]}`} 
               alt={pet.name} 
-              width="75px"
+              width="px75"
             />
             <span className="bold">{pet.name}</span>
-            <div className={styles.action}>
+            <div className={styles.actions}>
               {pet.available 
               ? (
                 <>
                   {pet.adopter && (
-                    <button>Concluir adoção</button>
+                    <button className={styles.conclude_btn}>Concluir adoção</button>
                   )}
                   <Link to={`/pet/edit/${pet._id}`}>Editar</Link> 
                   <button>Excluir</button>
