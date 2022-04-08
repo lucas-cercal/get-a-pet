f0149974671149dc9c4d2277edd540e7b0a44946
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-04-08 14:42:52 -0300

    Terminando formulário de pet

--- frontend/src/components/form/PetForm.js
+++ frontend/src/components/form/PetForm.js
@@ -11,19 +11,43 @@ export default function PetForm({handleSubmit, petData, btnText}) {
   const colors = ['Branco', 'Preto', 'Cinza', 'Caramelo', 'Mesclado']
 
   function onFileChange(e) {
-
+    setPreview(Array.from(e.target.files))
+    setPet({...pet, images: [...e.target.files]})
   }
 
   function handleChange(e) {
-
+    setPet({...pet, [e.target.name]: e.target.value})
   }
 
   function handleColor(e) {
+    setPet({...pet, color: e.target.options[e.target.selectedIndex].text})
+  }
 
+  function submit(e) {
+    e.preventDefault()
+    console.log(pet)
+    // handleSubmit(pet)
   }
 
   return (
-    <form className={formStyles.form_container}>
+    <form onSubmit={submit} className={formStyles.form_container}>
+      <div className={formStyles.preview_pet_images}>
+        {preview.length > 0 ? preview.map((image, index) => (
+          <img 
+            src={URL.createObjectURL(image)} 
+            alt={pet.name} 
+            key={`${pet.name}+${index}`}
+          />
+        )) : 
+        pet.images && pet.images.map((image, index) => (
+          <img 
+            src={`${process.env.REACT_APP_API}/images/pets/${image}`} 
+            alt={pet.name} 
+            key={`${pet.name}+${index}`}
+          />
+        ))
+      }
+      </div>
       <Input
         text="Imagens do Pet"
         type="file"
