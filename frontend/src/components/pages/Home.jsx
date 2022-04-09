d0b481f60b20ef7a2c84f54a5b07650b50b911e2
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-04-09 17:42:45 -0300

    CSS da home

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -16,15 +16,20 @@ export default function Home() {
 
   return (
     <section>
-      <div>
+      <div className={styles.pet_home_header}>
         <h1>Adote um Pet</h1>
         <p>Veja os detalhes de cada um e conheça o tutor deles</p>
       </div>
-      <div>
+      <div className={styles.pet_container}>
         {pets.length > 0 ? 
           pets.map((pet) => (
-            <div>
-              <p>Imagem do Pet</p>
+            <div className={styles.pet_card}>
+              <div
+                style={{
+                  backgroundImage: `url(${process.env.REACT_APP_API}/images/pets/${pet.images[0]})`,
+                }}
+                className={styles.pet_card_image}
+              ></div>
               <h3>{pet.name}</h3>
               <p>
                 <span className="bold">Peso:</span> {pet.weight}kg
@@ -32,7 +37,7 @@ export default function Home() {
               {pet.available ? (
                 <Link to={`pet/${pet._id}`}>Mais detalhes</Link>
               ) : (
-                <p>Adotado</p>
+                <p className={styles.adopted_text}>Adotado!</p>
               )}
           </div>
         )) 
