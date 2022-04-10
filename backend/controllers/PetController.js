c96a1ee7dba901fa25d2144c11fbf2faa458bc70
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-04-09 21:13:38 -0300

    Fix

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -215,10 +215,7 @@ module.exports = class PetController{
             updatedData.color = color;
         }
 
-        if(images.length === 0){
-            res.status(422).json({message: "A imagem é obrigatória!"});
-            return;
-        } else{
+        if(images.length > 0){
             updatedData.images = [];
             images.map((image) => {
                 updatedData.images.push(image.filename);
